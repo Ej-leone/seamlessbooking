91e98c9fb79dc83d8225d4d3ff0d858d9906c684
Ej <Ejgithinji@gmail.com>
2019-08-09 09:49:09 +0300

    minor adjustments

--- src/navigator/AppRouteConfigs.js
+++ src/navigator/AppRouteConfigs.js
@@ -1,9 +1,19 @@
 import {
     createStackNavigator,
 } from 'react-navigation';
+import Bookings from '../screens/Bookings'
+import Login from '../screens/Login'
+import Main from '../screens/Main'
+import MakeBook from '../screens/MakeBook'
+import Report from '../screens/Room'
+import Setting from '../screens/Setting'
+import Csignup from '../screens/Signup/Csignup'
+import Usignup from '../screens/Signup/Usignup'
+import screen from '../screens/Onboarding/screen'
 
 
 
-
-const AppRouteConfigs = createStackNavigator({})
+const AppRouteConfigs = createStackNavigator({
+    screen: { screen: screen }
+})
 export default AppRouteConfigs;
